dfb163ecefb1219d531bca53276ffdb8ef94cc1f
ickerio <harry.ickeringill@gmail.com>
2019-12-19 15:51:20 +0800

    Parsing a service

--- parsers/service.go
+++ parsers/service.go
@@ -1,9 +1,10 @@
 package parsers
 
 import (
+	"bufio"
 	"errors"
-	"fmt"
 	"os"
+	"strings"
 )
 
 type Command interface {
@@ -37,16 +38,54 @@ func (p PutFile) Run() error {
 }
 
 type Service struct {
-	commands []Command
+	Create []Command
+	Start  []Command
+	Stop   []Command
+	Backup []Command
 }
 
 func GetService(fileName string) (Service, error) {
+	service := Service{}
+	var current string
+
 	f, err := os.Open(fileName)
 	if err != nil {
-		return Service{}, errors.New("Could not find config file")
+		return Service{}, errors.New("Could not find service file")
 	}
 
-	fmt.Println(f)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		components := strings.Split(strings.TrimLeft(scanner.Text(), "\t \n"), ":")
 
-	return Service{}, nil
+		switch components[0] {
+		case "create", "start", "stop", "backup":
+			current = components[0]
+		case "exe":
+			command := Execute{data: components[1]}
+			addToService(current, command, &service)
+		case "get":
+			body := strings.Split(components[1], ",")
+			command := GetFile{source: body[0], destination: body[1]}
+			addToService(current, command, &service)
+		case "put":
+			body := strings.Split(components[1], ",")
+			command := PutFile{source: body[0], destination: body[1]}
+			addToService(current, command, &service)
+		}
+	}
+
+	return service, nil
+}
+
+func addToService(current string, command Command, service *Service) {
+	switch current {
+	case "create":
+		service.Create = append(service.Create, command)
+	case "start":
+		service.Start = append(service.Start, command)
+	case "stop":
+		service.Stop = append(service.Stop, command)
+	case "backup":
+		service.Backup = append(service.Backup, command)
+	}
 }
